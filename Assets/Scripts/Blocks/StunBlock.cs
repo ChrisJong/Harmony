d6891e328d3f13d04b9c87d15c6b901d03de30b6
Chris Jong <chris_jong@live.com.au>
2014-10-19 21:36:12 +1100

    Version 1.0.6

---
+++ Assets/Scripts/Blocks/StunBlock.cs
@@ -0,0 +1,19 @@
+﻿namespace Blocks {
+
+    using System.Collections.Generic;
+
+    using UnityEngine;
+
+    using GameInfo;
+
+    public class StunBlock : BlockClass {
+
+        public override void MoveUp() {
+            throw new System.NotImplementedException();
+        }
+
+        public override void MoveDown() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/ResourceController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+public class ResourceController : MonoBehaviour {
+
+    public static ResourceController instance;
+
+    public GameObject gameMenuController;
+    public GameObject gameController;
+    public GameObject gridController;
+    public GameObject soundController;
+
+    public GameObject playerObject;
+    public GameObject aiObject;
+
+    public GameObject levelNumberBlock;
+
+    public Material skyBox;
+
+    void Awake() {
+        instance = this;
+    }
+}
