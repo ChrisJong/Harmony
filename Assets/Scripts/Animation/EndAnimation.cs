8403c15ccc09afbd774453d355dd65c5b7fd3abd
Chris Jong <chris_jong@live.com.au>
2014-11-05 20:12:44 +1100

    Version - Jazz Hands (1.9.6.3)
    - Added in temp camera movement upwards for end animation

--- Assets/Scripts/Animation/EndAnimation.cs
+++ Assets/Scripts/Animation/EndAnimation.cs
@@ -6,10 +6,24 @@
     using Grid;
 
     public class EndAnimation : MonoBehaviour {
+
+        /*private bool _moveCamera;
+        private GameObject _mainCamera;
+
+        void Awake() {
+            this._mainCamera = Camera.main.gameObject;
+        }*/
+
         void Update() {
             if(!GridController.instance.EndMenuActive) {
                 this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y + 1.0f, this.transform.position.z), Time.deltaTime * 2.0f);
-                this.transform.Rotate(0.0f, 50.0f * Time.deltaTime, 0.0f); 
+                this.transform.Rotate(0.0f, 50.0f * Time.deltaTime, 0.0f);
+
+                /*if(this._moveCamera)
+                    this._mainCamera.transform.position = Vector3.Lerp(this._mainCamera.transform.position, new Vector3(this._mainCamera.transform.position.x, this._mainCamera.transform.position.y + this.transform.position.y, this._mainCamera.transform.position.z), Time.deltaTime * 0.5f);
+
+                if(this.transform.position.y > (Camera.main.transform.position.y - 3.0f))
+                    this._moveCamera = true;*/
             } else
                 Destroy(this.gameObject);
         }
